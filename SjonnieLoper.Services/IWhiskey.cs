e0adedf378f847725fbc27cfcd4b660845532f40
Jonathan Meibergen <jonathanmeibergen@outlook.com>
2020-11-05 15:51:51 +0100

    Sql and Mock Data separation for Whiskeys

--- SjonnieLoper.Services/IWhiskey.cs
+++ SjonnieLoper.Services/IWhiskey.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using SjonnieLoper.Core.Models;
 
 namespace SjonnieLoper.Services
@@ -13,55 +11,5 @@ namespace SjonnieLoper.Services
         Whiskey Update(Whiskey updatedWhiskey);
         Whiskey Create(Whiskey newWhiskey);
         int Commit();
-    }
-
-    public class Mock_Whiskey : IWhiskeys
-    {
-        private List<Whiskey> _whiskeys;
-
-        public Mock_Whiskey()
-        {
-            _whiskeys = new List<Whiskey>()
-            {
-                new Whiskey(
-                    1,
-                    "Grey Goose",
-                    20,
-                    "Austria",
-                    40,
-                    "foo/bar/baz.png",
-                    new WhiskeyType() {WhiskeyTypeId = 1, Name = "SomeType"})
-            };
-        }
-
-        public IEnumerable<Whiskey> AllWhiskeys() => _whiskeys;
-        public Whiskey WhiskeyById(int id) => 
-            _whiskeys.FirstOrDefault(w => w.WhiskeyId == id);
-
-        public IEnumerable<Whiskey> WhiskeyByType(string typeName) =>
-            _whiskeys.Select(w => w)
-                .Where(t => t.WhiskeyType.Name == typeName);
-
-        public Whiskey Update(Whiskey updatedWhiskey)
-        {
-            var reservation = 
-                _whiskeys.SingleOrDefault(r => r.WhiskeyId == updatedWhiskey.WhiskeyId);
-            return reservation != null
-                ? updatedWhiskey
-                : null;
-        }
-
-        public Whiskey Create(Whiskey newWhiskey)
-        {
-            _whiskeys.Add(newWhiskey);
-            newWhiskey.WhiskeyId = 
-                _whiskeys.Max(e => e.WhiskeyId) + 1;
-            return newWhiskey;
-        }
-
-        public int Commit()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
