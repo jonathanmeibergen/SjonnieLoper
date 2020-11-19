3b847732695c910866389f341fa99ffaa03c4617
Paul <pldbrn.117@gmail.com>
2020-11-19 01:50:20 +0100

    Added redis compose w/o personal conf.

--- MainApplication/Startup.cs
+++ MainApplication/Startup.cs
@@ -33,7 +33,7 @@ namespace SjonnieLoper
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DockerSqlConnection")));
             services.AddDefaultIdentity<ApplicationUser>(options =>
                     options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
